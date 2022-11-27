bafef6677c2b169429a7d800289d6bf61108dd04
vdthanh711 <thanh7112000@gmail.com>
2022-11-27 22:10:02 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="app" :class="typeof weather.main !='undefined' && weather.main.temp > 16 ? 'warm' : ''">
+    <main>
+      <div class="search-box">
+        <input type="text" class="search-bar" 
+        placeholder="Search..."
+        v-model= "query"
+        @keypress="fetchWeather"/>
+
+      </div>
+      <div class="weather-wrap" v-if=" typeof weather.main !='undefined'">
+        <div class="location-box">
+          <div class="location">{{weather.name}}, {{weather.sys.country}}</div>
+          <div class="date">{{dateBuilder()}}</div>
+        </div>
+        <div class="weather-box">
+          <div class="temp">{{Math.round(weather.main.temp)}}℃</div>
+          <div class="weather">{{weather.weather[0].main}}</div>
+      </div>
+      </div>
+      
+    </main>
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+    data(){
+      return{
+        api_key:'f6a2c317f97367bfe8fa3e5fc517e80d',
+        url_base:"https://api.openweathermap.org/data/2.5",
+        query: '',
+        weather: {}
+      }
+    },
+    methods: {
+      fetchWeather(e){
+        if(e.key == "Enter"){
+          fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+            .then(res=>{
+              return res.json();
+            }).then(this.setResults);
+        }
+      },
+      setResults(results){
+        this.weather = results;
+      },
+      dateBuilder (){
+        let d= new Date();
+        let months = ["January", "February", "March", "April", "May", "June", "July", "August","September", "October", "November", "December"];
+        let days = ["Sunday", "Monday", "Tueday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+        let day = days[d.getDay()];
+        let date = d.getDate();
+        let month = months[d.getMonth()];
+        let year = d.getFullYear();
+
+        return `${day} ${date} ${month} ${year}`;
+      }
+    }
+}
+</script>
+
+<style src="./style.css">
+ 
+
+</style>
