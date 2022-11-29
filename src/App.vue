23d09decec8925694e67b6762bee2a0f4a698998
Vu Duy Thanh <vd.thanh@samsung.com>
2022-11-29 16:08:00 +0700

    291122

--- src/App.vue
+++ src/App.vue
@@ -16,42 +16,54 @@
           <div class="location">{{weather.name}}, {{weather.sys.country}} </div>
           <div class="date">{{dateBuilder()}}</div>
         </div>
-        <div class="weather-box">
+        <div class="weather-box" >
           <div class="temp">{{Math.round(weather.main.temp)}}℃</div>
           <div class="weather">{{weather.weather[0].main}}</div>
       </div>
+        
       </div>
       
+      <div class="response">
+        <div>{{weather.message}}</div>
+        
+      </div>
     </main>
 
   </div>
 </template>
 
-<script>
-
+<script >
+ 
 
 export default {
+  
   name: 'App',
     data(){
       return{
         api_key:'d78fd1588e1b7c0c2813576ba183a667',
         url_base:"https://api.openweathermap.org/data/2.5/",
         query: '',
-        weather: {}
+        weather: [],
+        status:''
       }
     },
     methods: {
-       
-      
+         
       click(){
         fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
             .then(res=>{
-              return res.json();
-            }).then(this.setResults,console.log(this.weather));
+              res.json().then(res => {
+                // set res vào 1 state
+             
+                this.weather = res
+                console.log(this.weather);
+              })
+            }).catch(err => {
+              this.weather = err
+              console.log(err);
+            })
       },
-      setResults(results){
-        this.weather = results;
-      },
+     
       dateBuilder (){
         let d= new Date();
         let months = ["January", "February", "March", "April", "May", "June", "July", "August","September", "October", "November", "December"];
@@ -63,8 +75,8 @@ export default {
         let year = d.getFullYear();
 
         return `${day} ${date} ${month} ${year}`;
-      }
-      
+      },
+
     }
 }
 </script>
